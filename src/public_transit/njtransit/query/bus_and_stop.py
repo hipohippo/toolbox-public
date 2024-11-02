7a8392b3135fafc9d91b62a22b6c850570d3eaff
hipo <90704942+hipohippo@users.noreply.github.com>
2024-11-01 22:34:09 -0400

    njt update

--- src/public_transit/njtransit/query/bus_and_stop.py
+++ src/public_transit/njtransit/query/bus_and_stop.py
@@ -1,8 +1,8 @@
 import re
 from enum import Enum
-from typing import List
-
 import pandas as pd
+import logging
+BUS_NUMBERS = ["156R", "159R", "158"]
 
 
 class NJTBusStop(Enum):
@@ -16,33 +16,53 @@ class NJTBusStop(Enum):
 
     LHNY = "LH", "NY", 21831  ## lincoln harbor toward new york
     LHNJ = "LH", "NJ", 21830  ## lincoln harbor toward new jersey
-    PABT = "PABT", "NJ", 21830  ## port authority bus terminal toward new jersey
-    RWNY = "RW", "NY", 21923
+
+    RWNY = "RW", "NY", 21923  ## Port Imperial at Riverwalk Place
+    RWNJ = "RW", "NJ", 21921  ## Port Imperial at Riverwalk Place
+
+    PABT = "PABT", "NJ", 26229  ## port authority bus terminal toward new jersey
 
 
 class NextBus:
-    def __init__(self, bus_number: str, predicted_departure_str: str, vehicle_info: str):
+    def __init__(self, stop_id: NJTBusStop, bus_number: str, predicted_departure_str: str, vehicle_info: str):
         """
         :param stop:
         :param predicted_time:  must match the regex {[\d]+ MIN"
         :param bus_number:      <rn>
         """
+        self.stop_id = stop_id
         self.bus_number = bus_number
-        self.predicted_departure_min: int = re.match(r"[^\d]*([\d]+)[ ]*MIN", predicted_departure_str)[1]
-        self.departure_time = pd.Timestamp.now() + pd.Timedelta(f"{self.predicted_departure_min} min")
-        self.vehicle_info = vehicle_info
+        predicted_departure = re.match(r"[^\d]*([\d]+)[ ]*MIN", predicted_departure_str)
+        if predicted_departure:
+            self.predicted_departure_min = int(predicted_departure.group(1))
+            self.predicted_departure_str = str(predicted_departure_str)
+            self.departure_time = pd.Timestamp.now() + pd.Timedelta(f"{self.predicted_departure_min} min")
+            self.departure_time_str = self.departure_time.strftime("%I:%M %p")
+        else:
+            self.predicted_departure_min = None
+            self.predicted_departure_str = "DELAYED"
+            self.departure_time = None
+            self.departure_time_str = ""
+
+        grps = re.search(r".*(\(Passengers: \w\)).+", vehicle_info)
+        if grps:
+            self.passengers_info = grps.group(1)
+        else:
+            self.passengers_info = ""
+
+        if self.stop_id == NJTBusStop.PABT and self.departure_time:
+            if self.departure_time.time() > pd.Timestamp("2024-01-01 22:00:00").time():
+                self.gate = " Gate 301"
+            else:
+                self.gate = {"158": " Gate 202", "159R": "Gate 201-2", "156R": "Gate 201-3"}[self.bus_number]
+        else:
+            self.gate = ""
 
     def to_html(self) -> str:
-        return (
-            f"{self.bus_number}: <b>{self.predicted_departure_min} MIN</b> @ <b>{self.departure_time.strftime('%I:%M %p')} "
-            f"{self.vehicle_info}</b>"
+        logging.info(
+            f"{self.bus_number}{self.gate}: in <b>{self.predicted_departure_str}</b> @ <b>{self.departure_time_str} "
         )
-
-
-def format_bus_message(stop: NJTBusStop, bus_arrivals: List[NextBus], direction: str) -> str:
-    message = (f"To <b>{direction}</b> StopID={stop.id}:\n") + (
-        "\n".join([bus.to_html() for bus in bus_arrivals if bus.departure_time >= pd.Timestamp.now()])
-        if bus_arrivals
-        else "No bus found"
-    )
-    return message
+        return (
+            f"{self.bus_number}{self.gate}: in <b>{self.predicted_departure_str}</b> @ <b>{self.departure_time_str} "
+            f"{self.passengers_info}</b>"
+        )
--- src/public_transit/njtransit/query/bus_api.py
+++ src/public_transit/njtransit/query/bus_api.py
@@ -8,19 +8,26 @@ from typing import List
 from bs4 import BeautifulSoup
 from nodriver.core.browser import Browser
 
-from public_transit.njtransit.query.bus_and_stop import NJTBusStop, NextBus, format_bus_message
+from public_transit.njtransit.query.bus_and_stop import NJTBusStop, NextBus, BUS_NUMBERS
 
 
-async def extract_content_from_page(stop: NJTBusStop, browser: Browser) -> BeautifulSoup:
+def format_bus_message(stop: NJTBusStop, bus_arrivals: List[NextBus], direction: str) -> str:
+    message = (f"To <b>{direction}</b> StopID={stop.id}:\n") + (
+        "\n".join([bus.to_html() for bus in bus_arrivals]) if bus_arrivals else "No bus found"
+    )
+    return message
+
+
+async def extract_content_from_page(stop: NJTBusStop, busno: str, browser: Browser) -> BeautifulSoup:
     tab1 = await browser.get(
-        f"https://mybusnow.njtransit.com/bustime/wireless/html/eta.jsp?route=All&id={stop.id}&showAllBusses=on"
+        f"https://mybusnow.njtransit.com/bustime/wireless/html/eta.jsp?route={busno}&id={stop.id}&showAllBusses=off"
     )
     page_body = await tab1.query_selector("body")
     page_content = await page_body.get_html()
     return BeautifulSoup(page_content, features="lxml")
 
 
-def parse_content_from_page(page_content: BeautifulSoup) -> List[NextBus]:
+def parse_content_from_page(stop: NJTBusStop, page_content: BeautifulSoup) -> List[NextBus]:
     scheduled_bus_and_arrivals = page_content.find_all("strong", attrs={"class": "larger"})
     capacity = page_content.find_all("span", attrs={"class": "smaller"})
     bus_and_prediction = [element.text.replace("\xa0", "") for element in scheduled_bus_and_arrivals]
@@ -28,13 +35,15 @@ def parse_content_from_page(page_content: BeautifulSoup) -> List[NextBus]:
 
     num_arrival_info = len(bus_and_prediction) // 2
     return [
-        NextBus(bus_and_prediction[2 * i], bus_and_prediction[2 * i + 1], vehicle_info[i])
+        NextBus(stop, bus_and_prediction[2 * i], bus_and_prediction[2 * i + 1], vehicle_info[i])
         for i in range(num_arrival_info)
     ]
 
 
 async def next_bus_job(stop: NJTBusStop, direction: str, browser: Browser) -> str:
-    page_content: BeautifulSoup = await extract_content_from_page(stop, browser)
-    bus_arrivals = parse_content_from_page(page_content)
+    bus_arrivals = []
+    for busno in BUS_NUMBERS:
+        page_content: BeautifulSoup = await extract_content_from_page(stop, busno, browser)
+        bus_arrivals.extend(parse_content_from_page(stop, page_content))
     message = format_bus_message(stop, bus_arrivals, direction)
     return message
