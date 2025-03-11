c9729febadbe1d2716da8b9d2b1164097e344efc
hipo <hipo@github>
2025-03-11 00:48:30 -0400

    minor change

--- src/online_resource/ebook/sobooks_downloader.py
+++ src/online_resource/ebook/sobooks_downloader.py
@@ -1,8 +1,10 @@
 import re
+import traceback
 from pathlib import Path
 from typing import Tuple, List
 
 import requests
+from bs4 import BeautifulSoup
 
 from request_common.request_download_file import request_download
 
@@ -23,8 +25,8 @@ def get_download_url(bookid: int) -> Tuple[str, str, str]:
     isbn = re.search(r".*ISBN：</strong>(\d{13}).*", bookpage.text)[1]
     year = time_search[1]
     month = time_search[2]
-    book_title_search = re.search(r".*<title>(.*) by", bookpage.text)
-    book_title = book_title_search[1]
+    soup = BeautifulSoup(bookpage.text, parser="lxml")
+    book_title = soup.find(class_="article-title").text
     return (f"https://sobooks.cloud/{year}/{month}/{bookid}.epub", book_title, isbn)
 
 
@@ -32,11 +34,11 @@ def batch_download(ids: List[int], destination: Path):
     for bookid in ids:
         try:
             book_file_url, book_title, book_isbn = get_download_url(bookid)
-            request_download(book_file_url, destination)
+            request_download(book_file_url, destination / f"{book_isbn}_{book_title}.epub")
             print(f"downloaded {bookid}， {book_title}")
         except Exception as e:
-            print(f"failed to download {bookid}", e)
+            print(f"failed to download {bookid}", e, traceback.format_exc())
 
 
 if __name__ == "__main__":
-    batch_download([21497], ...)
+    batch_download([22270], )
